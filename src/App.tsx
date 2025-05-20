ae88262aa1ab63fadd734b7a7c7ab43e7811b55e
Arvind Anbuvel <aravinthan.anbuvel@gmail.com>
2025-05-20 15:53:08 +1000

    Basic chatbot with question tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Chatbot from './Chatbot';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="min-h-screen bg-slate-50 flex flex-col items-center justify-center p-4">
+      <h1 className="text-3xl font-bold mb-6 text-sky-900">Financial Report Chatbot</h1>
+      <Chatbot />
     </div>
   );
 }
